ee954fea75dda0052ddc36cc3b664aaf413b470f
Scott Ware <scottdware@gmail.com>
2014-03-28 17:41:45 -0400

    Updated install script

--- install.ps1
+++ install.ps1
@@ -6,8 +6,9 @@ $destination = "$($modulePath)\Posh-Junos"
 $client = New-Object System.Net.WebClient
 
 if (!(Test-Path -Path $poshSSH)) {
-    Write-Warning "Looks like you don't have 'Posh-SSH' installed...installing it now."
-    iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
+    Write-Warning "Looks like you don't have 'Posh-SSH' installed!"
+    Write-Warning "Please visit https://github.com/darkoperator/Posh-SSH to install it first!"
+    exit
 }
 
 if (Test-Path -Path $destination) {
