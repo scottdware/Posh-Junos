ec2c1b51eec3806884d41176771852e1a1053f30
Scott Ware <scottdware@gmail.com>
2014-03-28 17:38:10 -0400

    Updated install script

--- install.ps1
+++ install.ps1
@@ -7,7 +7,7 @@ $client = New-Object System.Net.WebClient
 
 if (!(Test-Path -Path $poshSSH)) {
     Write-Warning "Looks like you don't have 'Posh-SSH' installed...installing it now."
-    $client.DownloadString('https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev')
+    iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
 }
 
 if (Test-Path -Path $destination) {
