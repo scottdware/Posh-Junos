ea5de6317bcadef1c7d10fc8f55e9f5339a0f3ac
Scott Ware <scottdware@gmail.com>
2014-12-31 12:26:42 -0500

    Changed line

--- Posh-Junos.psm1
+++ Posh-Junos.psm1
@@ -295,25 +295,25 @@ function New-TrafficSelector {
     .Link
         https://github.com/scottdware/Posh-Junos/wiki/Functions#New-TrafficSelector
     #>
-    
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
         [string[]] $Local,
-        
+
         [Parameter(Mandatory = $true)]
         [string[]] $Remote,
-        
+
         [Parameter(Mandatory = $true)]
         [string] $VPN,
-        
+
         [Parameter(Mandatory = $false)]
         [string] $File
     )
-    
+
     $Total = $Local.Count * $Remote.Count
     $Start = 1
-        
+
     if ($File) {
         if (Test-Path $File) {
             $ans = Read-Host 'Log file exists. Do you wish to overwrite? [y/n]'
@@ -326,33 +326,33 @@ function New-TrafficSelector {
         else {
             New-Item -Path $File -ItemType file | Out-Null
         }
-        
+
         Log-Output -File $File -Content "configure"
-        
+
         while ($Start -le $Total) {
             foreach ($localIP in $Local) {
                 foreach ($remoteIP in $Remote) {
                     # $selector = "set security ipsec vpn $VPN traffic-selector $($localIP)_$($remoteIP) local-ip $localIP remote-ip $remoteIP"
                     $selector = "set security ipsec vpn $VPN traffic-selector TS$($Start) local-ip $localIP remote-ip $remoteIP"
                     Log-Output -File $File -Content $selector
-                    
+
                     $Start += 1
                 }
             }
         }
-        
+
         Log-Output -File $File -Content "commit and-quit"
     }
-    
+
     else {
         Write-Output "-- Copy & Paste into SRX --`n"
-        
+
         while ($Start -le $Total) {
             foreach ($localIP in $Local) {
                 foreach ($remoteIP in $Remote) {
                     # Write-Output "set security ipsec vpn $VPN traffic-selector $($localIP)_$($remoteIP) local-ip $localIP remote-ip $remoteIP"
                     Write-Output "set security ipsec vpn $VPN traffic-selector TS$($Start) local-ip $localIP remote-ip $remoteIP"
-                    
+
                     $Start += 1
                 }
             }
